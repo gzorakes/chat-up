6ef3fd06d2b196483a4e5afe2e4c8ff30a271aee
zorakisgeorge <zorakisgeorge@gmail.com>
2025-09-19 00:01:42 +0300

    login & logout & update profile - auth routes

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    // get token from cookies with package cookie-parser
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized, No token provided" });
+    }
+    // verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized, Invalid token" });
+    }
+
+    // get user from token
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    // attach user to request object
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error in auth middleware:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
